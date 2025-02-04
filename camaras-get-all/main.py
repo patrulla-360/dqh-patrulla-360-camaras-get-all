ec5c601e4361c5c2f71f9e2f7668776b5a40ab73
patrulla-360 <patrullas360@gmail.com>
2025-02-03 23:37:04 -0300

    Update main.py

--- camaras-get-all/main.py
+++ camaras-get-all/main.py
@@ -18,4 +18,4 @@ async def startup():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main_d_camara:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
